4feb2ed13972b9b2f210e38432d3b2f2f5a10269
Brandon Benham <brandon.benham@dxc.com>
2023-12-24 12:35:07 -0600

    Brute force no leak

--- src/main/java/com/example/demo/service/TwoSumService.java
+++ src/main/java/com/example/demo/service/TwoSumService.java
@@ -31,7 +31,7 @@ public class TwoSumService
 {
    @Autowired
    private Config config;
-   private static Map< String, String > leaker = new HashMap<>();
+   // private static Map< String, String > leaker = new HashMap<>();
    // private static List< Double > leaker = new ArrayList<>();
 
    /**
@@ -76,11 +76,11 @@ public class TwoSumService
     */
    private TwoSumResponse bruteForce( List< Long > numbers, long target )
    {
-      leaker.put( randomUUID().toString(), randomUUID().toString() );
+      // leaker.put( randomUUID().toString(), randomUUID().toString() );
 
       for( int i=0; i<numbers.size(); i++ )
       {
-         leaker.put( randomUUID().toString(), "0" );
+         // leaker.put( randomUUID().toString(), "0" );
          for( int j=i+1; j<numbers.size(); j++ )
          {
             if( numbers.get( i ) + numbers.get( j ) == target )
