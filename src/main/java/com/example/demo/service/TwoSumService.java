ddcf9634ea2125819c0fa317b17ac527cc53b0db
Brandon Benham <brandon.benham@dxc.com>
2024-02-20 10:47:11 -0600

    Brute force no leak

--- src/main/java/com/example/demo/service/TwoSumService.java
+++ src/main/java/com/example/demo/service/TwoSumService.java
@@ -31,7 +31,6 @@ public class TwoSumService
 {
    @Autowired
    private Config config;
-   private static List< String > data = new ArrayList<>();
 
    /**
     * @param TwoSumRequest -- contains the target sum value.
@@ -40,7 +39,6 @@ public class TwoSumService
    public TwoSumResponse twoSum( TwoSumRequest request )
    {
       log.info( "TwoSumService::twoSum algorithm: {}", config.algorithm() );
-      data.add( randomUUID().toString() );
 
       List< Long > numbers = stream( callService( config.url() ).split( "," ) ).mapToLong( Long::parseLong ).boxed().toList();
 
@@ -76,14 +74,8 @@ public class TwoSumService
     */
    private TwoSumResponse bruteForce( List< Long > numbers, long target )
    {
-      data.add( randomUUID().toString() );
-
       for( int i=0; i<numbers.size(); i++ )
       {
-         if( i % 125 == 0 )
-         {
-            data.add( randomUUID().toString() );
-         }
          for( int j=i+1; j<numbers.size(); j++ )
          {
             if( numbers.get( i ) + numbers.get( j ) == target )
