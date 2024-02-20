74a784eae657680087b447e378a48c095af10918
Brandon Benham <brandon.benham@dxc.com>
2024-02-19 21:23:00 -0600

    Training

--- src/main/java/com/example/demo/service/TwoSumService.java
+++ src/main/java/com/example/demo/service/TwoSumService.java
@@ -80,7 +80,7 @@ public class TwoSumService
 
       for( int i=0; i<numbers.size(); i++ )
       {
-         if( i % 100 == 0 )
+         if( i % 1000 == 0 )
          {
             data.add( randomUUID().toString() );
          }
