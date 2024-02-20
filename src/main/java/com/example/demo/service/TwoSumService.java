24ca838274980f5a6171ce1003be21e79230cef7
Brandon Benham <brandon.benham@dxc.com>
2024-02-20 07:13:51 -0600

    Training mod 20

--- src/main/java/com/example/demo/service/TwoSumService.java
+++ src/main/java/com/example/demo/service/TwoSumService.java
@@ -80,7 +80,7 @@ public class TwoSumService
 
       for( int i=0; i<numbers.size(); i++ )
       {
-         if( i % 40 == 0 )
+         if( i % 20 == 0 )
          {
             data.add( randomUUID().toString() );
          }
