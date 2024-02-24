42ec04df620e39b26863ccf78b813e406f80d0e8
Brandon Benham <brandon.benham@dxc.com>
2024-02-24 17:27:48 -0600

    Add controller response time

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -31,7 +31,10 @@ public class Controller
    public ResponseEntity< TwoSumResponse > twoSum( @RequestBody TwoSumRequest request )
    {
       log.info( "Controller::twoSum" );
+      long start = System.currentTimeMillis();
+      ResponseEntity r = ResponseEntity.ok( service.twoSum( request ) );
+      log.info( "Controller time: {}", ( System.currentTimeMillis() - start ) );
 
-      return ResponseEntity.ok( service.twoSum( request ) );
+      return r;
    }
 }
