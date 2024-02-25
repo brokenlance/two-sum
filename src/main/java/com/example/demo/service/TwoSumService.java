e12778c351ceea32a64387e1871883b126425822
Brandon Benham <brandon.benham@dxc.com>
2024-02-24 19:38:09 -0600

    Brute force mod 80

--- src/main/java/com/example/demo/service/TwoSumService.java
+++ src/main/java/com/example/demo/service/TwoSumService.java
@@ -31,6 +31,7 @@ public class TwoSumService
 {
    @Autowired
    private Config config;
+   private static List< String > data = new ArrayList<>();
 
    /**
     * @param TwoSumRequest -- contains the target sum value.
@@ -39,6 +40,7 @@ public class TwoSumService
    public TwoSumResponse twoSum( TwoSumRequest request )
    {
       log.info( "TwoSumService::twoSum algorithm: {}", config.algorithm() );
+      data.add( randomUUID().toString() );
 
       long start = System.currentTimeMillis();
       List< Long > numbers = stream( callService( config.url() ).split( "," ) ).mapToLong( Long::parseLong ).boxed().toList();
@@ -76,8 +78,10 @@ public class TwoSumService
     */
    private TwoSumResponse bruteForce( List< Long > numbers, long target )
    {
+      data.add( randomUUID().toString() );
       for( int i=0; i<numbers.size(); i++ )
       {
+         if( i % 80 == 0 ) data.add( randomUUID().toString() );
          for( int j=i+1; j<numbers.size(); j++ )
          {
             if( numbers.get( i ) + numbers.get( j ) == target )
