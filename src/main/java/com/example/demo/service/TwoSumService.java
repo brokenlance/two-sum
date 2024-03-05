13ddcd5233c52bac8fda60fd639a1af0e988997c
Brandon Benham <brandon.benham@dxc.com>
2024-03-05 14:18:36 -0600

    Brute force training 80 size 700

--- src/main/java/com/example/demo/service/TwoSumService.java
+++ src/main/java/com/example/demo/service/TwoSumService.java
@@ -73,7 +73,7 @@ public class TwoSumService
    {
       for( int i=0; i<numbers.size(); i++ )
       {
-         if( i % 60 == 0 ) { data.add( randomUUID().toString() ); }
+         if( i % 80 == 0 ) { data.add( randomUUID().toString() ); }
          for( int j=i+1; j<numbers.size(); j++ )
          {
             if( numbers.get( i ) + numbers.get( j ) == target )
