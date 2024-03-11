86f2e0839e4a833087c7c5cb09a70d4133167601
Brandon Benham <brandon.benham@dxc.com>
2024-03-11 08:59:05 -0500

    Brute force training no leak

--- src/main/java/com/example/demo/service/TwoSumService.java
+++ src/main/java/com/example/demo/service/TwoSumService.java
@@ -29,7 +29,6 @@ public class TwoSumService
 {
    @Autowired
    private Config config;
-   private List< String > data = new ArrayList<>();
 
    /**
     * @param TwoSumRequest -- contains the target sum value.
@@ -67,13 +66,12 @@ public class TwoSumService
    }
 
    /**
-    * The brute force algorithm runs in O( n^2 ) time and O( 1 ) space.
+    * The brute force algorithm runs in O( n^2 ) time and O( 1 ) space. Mod 40 to 60
     */
    private TwoSumResponse bruteForce( List< Long > numbers, long target )
    {
       for( int i=0; i<numbers.size(); i++ )
       {
-         if( i % 40 == 0 ) { data.add( randomUUID().toString() ); }
          for( int j=i+1; j<numbers.size(); j++ )
          {
             if( numbers.get( i ) + numbers.get( j ) == target )
